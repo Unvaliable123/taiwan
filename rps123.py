27bb4bff02d1083426f3f3d8e827564c8fb93068
Unvaliable123 <95622892+Unvaliable123@users.noreply.github.com>
2021-12-20 20:03:34 +0800

    Update rps123.py

--- rps123.py
+++ rps123.py
@@ -23,4 +23,4 @@ while True:
         if computer_action == "paper":
             st.write("Scissors cuts paper! You win!")
         else:
-            st.write("Rock smashes scissors! You lose."
+            st.write("Rock smashes scissors! You lose.")
