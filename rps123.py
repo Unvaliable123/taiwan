4fc86100fb8d9ad5fd3a7a94635e6182026b1737
Unvaliable123 <95622892+Unvaliable123@users.noreply.github.com>
2021-12-20 20:26:28 +0800

    Update rps123.py

--- rps123.py
+++ rps123.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import random
 
-title = st.text_input('Enter a choice, (rock, paper, scissors):', 'rock', 'paper', 'scissor')
+title = st.text_input('Enter a choice, (rock, paper, scissors):', 'rock')
 st.write('The choice is', title)
 
 '''
