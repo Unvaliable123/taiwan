9393510879975dd6a7d624f79b5ba34a05984d2e
Unvaliable123 <95622892+Unvaliable123@users.noreply.github.com>
2021-12-27 20:50:04 +0800

    Update rps123.py

--- rps123.py
+++ rps123.py
@@ -1,38 +1,33 @@
 import streamlit as st
 import random
 
-while True:
-    user_action = st.text_input('Enter a choice, (rock, paper, scissors):', '')
+user_action = st.text_input('Enter a choice, (rock, paper, scissors):', '')
 
-    confirm_input = st.button('輸入確認')
-    if confirm_input:
+confirm_input = st.button('輸入確認')
+if confirm_input:
     
     
     
     
-        possible_actions = ["rock", "paper", "scissors"]
-        computer_action = random.choice(possible_actions)
-        st.write('Player choice is', user_action)  
+    possible_actions = ["rock", "paper", "scissors"]
+    computer_action = random.choice(possible_actions)
+    st.write('Player choice is', user_action)  
    
-        if user_action == computer_action:
-            st.write(f"Both players selected {user_action}. It's a tie!")
-        elif user_action == "rock":
-            if computer_action == "scissors":
-                st.write("Rock smashes scissors! You win!")
-            else:
-                st.write("Paper covers rock! You lose.")
-        elif user_action == "paper":
-            if computer_action == "rock":
-                st.write("Paper covers rock! You win!")
-            else:
-                st.write("Scissors cuts paper! You lose.")
-        elif user_action == "scissors":
-            if computer_action == "paper":
-                st.write("Scissors cuts paper! You win!")
-            else:
-                st.write("Rock smashes scissors! You lose.")
+    if user_action == computer_action:
+        st.write(f"Both players selected {user_action}. It's a tie!")
+    elif user_action == "rock":
+        if computer_action == "scissors":
+            st.write("Rock smashes scissors! You win!")
+        else:
+            st.write("Paper covers rock! You lose.")
+    elif user_action == "paper":
+        if computer_action == "rock":
+            st.write("Paper covers rock! You win!")
+        else:
+            st.write("Scissors cuts paper! You lose.")
+    elif user_action == "scissors":
+        if computer_action == "paper":
+            st.write("Scissors cuts paper! You win!")
+        else:
+            st.write("Rock smashes scissors! You lose.")
                 
-        play_again = st.text_input('Play again? (y/n):', '')
-        if play_again.lower() != "y":
-            break
-          
