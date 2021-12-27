a7ca9bee823faaf0ca6bd98a5f325fb39c0c5387
Unvaliable123 <95622892+Unvaliable123@users.noreply.github.com>
2021-12-27 20:28:46 +0800

    Update rps123.py

--- rps123.py
+++ rps123.py
@@ -2,6 +2,7 @@ import streamlit as st
 import random
 
 user_action = st.text_input('Enter a choice, (rock, paper, scissors):', '')
+
 confirm_input = st.button('輸入確認')
     if confirm_input:
     
@@ -12,20 +13,20 @@ confirm_input = st.button('輸入確認')
         computer_action = random.choice(possible_actions)
         st.write('Player choice is', user_action)  
    
-    if user_action == computer_action:
-        st.write(f"Both players selected {user_action}. It's a tie!")
-    elif user_action == "rock":
-        if computer_action == "scissors":
-            st.write("Rock smashes scissors! You win!")
-        else:
-            st.write("Paper covers rock! You lose.")
-    elif user_action == "paper":
-        if computer_action == "rock":
-            st.write("Paper covers rock! You win!")
-        else:
-            st.write("Scissors cuts paper! You lose.")
-    elif user_action == "scissors":
-        if computer_action == "paper":
-            st.write("Scissors cuts paper! You win!")
-        else:
-            st.write("Rock smashes scissors! You lose.")
+        if user_action == computer_action:
+            st.write(f"Both players selected {user_action}. It's a tie!")
+        elif user_action == "rock":
+            if computer_action == "scissors":
+                st.write("Rock smashes scissors! You win!")
+            else:
+                st.write("Paper covers rock! You lose.")
+        elif user_action == "paper":
+            if computer_action == "rock":
+                st.write("Paper covers rock! You win!")
+            else:
+                st.write("Scissors cuts paper! You lose.")
+        elif user_action == "scissors":
+            if computer_action == "paper":
+                st.write("Scissors cuts paper! You win!")
+            else:
+                st.write("Rock smashes scissors! You lose.")
