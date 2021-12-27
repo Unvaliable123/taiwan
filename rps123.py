4a85eedd6d345424b4dc1277400bc209d8e08085
Unvaliable123 <95622892+Unvaliable123@users.noreply.github.com>
2021-12-27 20:45:01 +0800

    Update rps123.py

--- rps123.py
+++ rps123.py
@@ -32,7 +32,7 @@ while True:
             else:
                 st.write("Rock smashes scissors! You lose.")
                 
-        play_again = st.text_input('Play again? (y/n): ','')
-        if play_again.lower() != "y":
+        play_again = st.text_input('Play again? (y/n):', '')
+        if play_again.lower() == "y":
             break
           
