1b23aba97994a288d55bc842fef0caff536db5a9
Unvaliable123 <95622892+Unvaliable123@users.noreply.github.com>
2022-01-03 20:14:55 +0800

    Update rps123.py

--- rps123.py
+++ rps123.py
@@ -19,22 +19,22 @@ if confirm_input:
     elif user_action == "rock":
         if computer_action == "scissors":
             st.write("Computer choice scissor,rock smashes scissors! You win!")
-            st.title('🗿>✂ 😎')
+            st.title('🗿>✂   😎')
         else:
             st.write("Computer choice paper,paper covers rock! You lose.")
-            st.title('🗿<📄 😩')
+            st.title('🗿<📄   😩')
     elif user_action == "paper":
         if computer_action == "rock":
             st.write("Computer choice rock,paper covers rock! You win!")
-            st.title('📄>🗿 😎')
+            st.title('📄>🗿   😎')
         else:
             st.write("Computer choice scissors,scissors cuts paper! You lose.")
-            st.title('📄<✂ 😩')
+            st.title('📄<✂   😩')
     elif user_action == "scissors":
         if computer_action == "paper":
             st.write("Computer choice paper,scissors cuts paper! You win!")
-            st.title('✂>📄 😎')
+            st.title('✂>📄   😎')
         else:
             st.write("Computer choice rock,rock smashes scissors! You lose.")
-            st.title('✂<🗿 😩')
+            st.title('✂<🗿   😩')
                 
