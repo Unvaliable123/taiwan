c6b1f939b5bac073fb75c53e1e47d3ee050da6d3
Unvaliable123 <95622892+Unvaliable123@users.noreply.github.com>
2022-01-03 19:24:22 +0800

    Update rps123.py

--- rps123.py
+++ rps123.py
@@ -17,17 +17,17 @@ if confirm_input:
         st.write(f"Both players selected {user_action}. It's a tie!")
     elif user_action == "rock":
         if computer_action == "scissors":
-            st.write("Computer choice scissor,Rock smashes scissors! You win!")
+            st.write("Computer choice scissor,rock smashes scissors! You win!")
         else:
-            st.write("Computer choice scissor,Paper covers rock! You lose.")
+            st.write("Computer choice scissor,paper covers rock! You lose.")
     elif user_action == "paper":
         if computer_action == "rock":
-            st.write("Computer choice rock,Paper covers rock! You win!")
+            st.write("Computer choice rock,paper covers rock! You win!")
         else:
-            st.write("Computer choice rock,Scissors cuts paper! You lose.")
+            st.write("Computer choice rock,scissors cuts paper! You lose.")
     elif user_action == "scissors":
         if computer_action == "paper":
-            st.write("Computer choice paper,Scissors cuts paper! You win!")
+            st.write("Computer choice paper,scissors cuts paper! You win!")
         else:
-            st.write("Computer choice paper,Rock smashes scissors! You lose.")
+            st.write("Computer choice paper,rock smashes scissors! You lose.")
                 
